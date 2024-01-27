62263ee3dd2d063011856397548172cd62250b8e
Codcu-entel <88192139+Codcu-entel@users.noreply.github.com>
2024-01-27 22:30:32 +0300

    Serilog İle Loglama ve Seq İle Görselleştirme

--- Infrastructure/ETicaretAPI.Persistence/Services/AuthService.cs
+++ Infrastructure/ETicaretAPI.Persistence/Services/AuthService.cs
@@ -64,7 +64,7 @@ namespace ETicaretAPI.Persistence.Services
 			{
 				await _userManager.AddLoginAsync(user, info); //AspNetUserLogins
 
-				Token token = _tokenHandler.CreateAccessToken(accessTokenLifeTime);
+				Token token = _tokenHandler.CreateAccessToken(accessTokenLifeTime, user);
 				await _userService.UpdateRefreshToken(token.RefreshToken, user, token.Expiration, 15);
 				return token;
 			}
@@ -120,7 +120,7 @@ namespace ETicaretAPI.Persistence.Services
 			SignInResult result = await _signInManager.CheckPasswordSignInAsync(user, password, false);
 			if (result.Succeeded) //Authentication başarılı!
 			{
-				Token token = _tokenHandler.CreateAccessToken(accessTokenLifeTime);
+				Token token = _tokenHandler.CreateAccessToken(accessTokenLifeTime, user);
 				return token;
 			}
 			throw new AuthenticationErrorException();
@@ -131,7 +131,7 @@ namespace ETicaretAPI.Persistence.Services
 			AppUser? user = await _userManager.Users.FirstOrDefaultAsync(u => u.RefreshToken == refreshToken);
 			if (user != null && user?.RefreshTokenEndDate > DateTime.UtcNow)
 			{
-				Token token = _tokenHandler.CreateAccessToken(15);
+				Token token = _tokenHandler.CreateAccessToken(15, user);
 				await _userService.UpdateRefreshToken(token.RefreshToken, user, token.Expiration, 15);
 				return token;
 			}

f2e97dbe32158606535da3685854b23de87f6875
Codcu-entel <88192139+Codcu-entel@users.noreply.github.com>
2024-02-13 19:21:35 +0300

    Alışveriş Sepeti Altyapısı Geliştirme

--- Presentation/ETicaretAPI.API/Program.cs
+++ Presentation/ETicaretAPI.API/Program.cs
@@ -22,6 +22,7 @@ using ETicaretAPI.SignalR;
 
 var builder = WebApplication.CreateBuilder(args);
 
+builder.Services.AddHttpContextAccessor();//Client'tan gelen request neticesinde oluþturulan HttpContext nesnesine katmanlardaki class'lar üzerinden(busineess logic) eriþebilmemizi saðlayan bir servistir.
 builder.Services.AddPersistenceServices();
 builder.Services.AddInfrastructureServices();
 builder.Services.AddApplicationServices();
